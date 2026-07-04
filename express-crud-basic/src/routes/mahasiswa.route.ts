815eac65a9110e855e0d87b62e14e1a6f2ac0cea
izzudin al qassam <izzudinalqassam@if.uai.ac.id>
2026-07-04 11:40:46 +0700

    T14: role mahasiswa

--- express-crud-basic/src/routes/mahasiswa.route.ts
+++ express-crud-basic/src/routes/mahasiswa.route.ts
@@ -7,12 +7,38 @@ import {
 } from "../controllers/mahasiswa.controller";
 import { uploadFotoMahasiswa } from "../middlewares/upload.middleware";
 import { authMiddleware } from "../middlewares/auth.middleware";
+import { allowRoles } from "../middlewares/role.middleware";
  
 const router = Router();
  
-router.get("/", authMiddleware, getAllMahasiswa);
-router.post("/", authMiddleware, uploadFotoMahasiswa.single("foto"), createMahasiswa);
-router.put("/:id", authMiddleware, uploadFotoMahasiswa.single("foto"), updateMahasiswa);
-router.delete("/:id", authMiddleware, deleteMahasiswa);
+router.get(
+  "/",
+  authMiddleware,
+  allowRoles("admin", "operator", "viewer"),
+  getAllMahasiswa
+);
+ 
+router.post(
+  "/",
+  authMiddleware,
+  allowRoles("admin", "operator"),
+  uploadFotoMahasiswa.single("foto"),
+  createMahasiswa
+);
+ 
+router.put(
+  "/:id",
+  authMiddleware,
+  allowRoles("admin", "operator"),
+  uploadFotoMahasiswa.single("foto"),
+  updateMahasiswa
+);
+ 
+router.delete(
+  "/:id",
+  authMiddleware,
+  allowRoles("admin"),
+  deleteMahasiswa
+);
  
 export default router;
